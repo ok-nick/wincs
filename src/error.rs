66086559bbf176e4661b78d14abbeebf32eb01b5
ok-nick <25470747+ok-nick@users.noreply.github.com>
2022-05-15 11:21:54 -0400

    doc errors

--- src/error.rs
+++ src/error.rs
@@ -1,37 +1,70 @@
 use windows::Win32::Foundation::{self, NTSTATUS};
 
+/// Predefined error types provided by the operating system.
 #[derive(Debug, Clone, Copy)]
 pub enum CloudErrorKind {
+    /// Access to the cloud file is denied.
     AccessDenied,
+    /// The cloud sync root is already connected with another cloud sync provider.
     AlreadyConnected,
+    /// The cloud sync provider failed user authentication.
     AuthenticationFailed,
+    /// The operation is reserved for a connected cloud sync provider.
     ConnectedProviderOnly,
+    /// Dehydration of the cloud file is disallowed by the cloud sync provider.
     DehydrationDisallowed,
+    /// The cloud operation cannot be performed on a file with incompatible hardlinks.
     IncompatibleHardlinks,
+    /// The cloud sync provider failed to perform the operation due to low system resources.
     InsufficientResources,
+    /// The cloud operation is invalid.
     InvalidRequest,
+    /// The operation cannot be performed on cloud files in use.
     InUse,
+    /// The cloud file metadata is corrupt and unreadable.
     MetadataCorrupt,
+    /// The cloud file metadata is too large.
     MetadataTooLarge,
+    /// The cloud sync provider failed to perform the operation due to network being unavailable.
     NetworkUnavailable,
+    /// The file is not in sync with the cloud.
     NotInSync,
+    /// The operation is not supported by the cloud sync provider.
     NotSupported,
+    /// The operation is only supported on files under a cloud sync root.
     NotUnderSyncRoot,
+    /// The operation cannot be performed on pinned cloud files.
     Pinned,
+    /// The cloud file property is possibly corrupt. The on-disk checksum does not match the
+    /// computed checksum.
     PropertyBlobChecksumMismatch,
+    /// The cloud file property is too large.
     PropertyBlobTooLarge,
+    /// The cloud file's property store is corrupt.
     PropertyCorrupt,
+    /// The operation failed due to a conflicting cloud file property lock.
     PropertyLockConflict,
+    /// The version of the cloud file property store is not supported.
     PropertyVersionNotSupported,
+    /// The cloud file provider is not running.
     ProviderNotRunning,
+    /// The cloud file provider exited unexpectedly.
     ProviderTerminated,
+    /// The cloud operation is not supported on a read-only volume.
     ReadOnlyVolume,
+    /// The cloud operation was aborted.
     RequestAborted,
+    /// The cloud operation was canceled by user.
     RequestCancelled,
+    /// The cloud operation was not completed before the time-out period expired.
     RequestTimeout,
+    /// The cloud sync root metadata is corrupted.
     SyncRootMetadataCorrupt,
+    /// The maximum number of cloud file properties has been reached.
     TooManyPropertyBlobs,
+    /// The cloud operation was unsuccessful.
     Unsuccessful,
+    /// The cloud sync provider failed to validate the downloaded data.
     ValidationFailed,
 }
 
