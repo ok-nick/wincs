cb7c397efbf0e92981effcbf31440a8327f8fbd6
ok-nick <25470747+ok-nick@users.noreply.github.com>
2022-05-14 22:08:16 -0400

    remove empty line

--- src/root/connect.rs
+++ src/root/connect.rs
@@ -36,7 +36,6 @@ impl Connection {
     /// is scanning file system activity on files within the sync root.
     ///
     /// A call to `hydrate` from the `FileExt` trait will not be blocked by this flag.
-
     pub fn block_implicit_hydration(mut self) -> Self {
         self.0 |= CloudFilters::CF_CONNECT_FLAG_BLOCK_SELF_IMPLICIT_HYDRATION;
         self
