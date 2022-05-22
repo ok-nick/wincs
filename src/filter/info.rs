b6fde73ac3b489c9863a937ae1a3f7fd1fd40df7
ok-nick <25470747+ok-nick@users.noreply.github.com>
2022-05-22 19:21:13 -0400

    Added docs

--- src/filter/info.rs
+++ src/filter/info.rs
@@ -9,49 +9,68 @@ use windows::Win32::Storage::CloudFilters::{
     CF_CALLBACK_PARAMETERS_0_8, CF_CALLBACK_PARAMETERS_0_9,
 };
 
+/// Information for the `SyncFilter::fetch_data` callback.
 #[derive(Debug, Clone, Copy)]
 pub struct FetchData(pub(crate) CF_CALLBACK_PARAMETERS_0_6);
 
 impl FetchData {
-    pub fn recovery(&self) -> bool {
+    /// Whether or not the callback was called from an interrupted hydration.
+    pub fn interrupted_hydration(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_FETCH_DATA_FLAG_RECOVERY).0 != 0
     }
 
+    /// Whether or not the callback was called from an explicit hydration via `FileExt::hydrate`.
     pub fn explicit_hydration(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_FETCH_DATA_FLAG_EXPLICIT_HYDRATION).0 != 0
     }
 
+    // TODO: does this amount always lay on 4kb or EoF?
+    /// The amount of bytes that must be written to the placeholder.
     pub fn required_file_range(&self) -> Range<u64> {
         (self.0.RequiredFileOffset as u64)
             ..(self.0.RequiredFileOffset + self.0.RequiredLength) as u64
     }
 
+    // TODO: what is this field
+    // https://docs.microsoft.com/en-us/answers/questions/748214/what-is-fetchdataoptionalfileoffset-cfapi.html
     pub fn optional_file_range(&self) -> Range<u64> {
         (self.0.OptionalFileOffset as u64)
             ..(self.0.OptionalFileOffset + self.0.OptionalLength) as u64
     }
 
+    /// The last time the file was dehydrated.
+    ///
+    /// This value is a count of 100-nanosecond intervals since January 1, 1601.
     pub fn last_dehydration_time(&self) -> u64 {
         self.0.LastDehydrationTime as u64
     }
 
-    pub fn last_dehydration_reason(&self) -> DehydrationReason {
-        self.0.LastDehydrationReason.into()
+    /// The reason the file was last dehydrated.
+    pub fn last_dehydration_reason(&self) -> Option<DehydrationReason> {
+        DehydrationReason::from_win32(self.0.LastDehydrationReason)
     }
 }
 
+/// Information for the `SyncFilter::cancel_fetch_data` callback.
 #[derive(Clone, Copy)]
 pub struct CancelFetchData(pub(crate) CF_CALLBACK_PARAMETERS_0_0);
 
 impl CancelFetchData {
-    pub fn io_timeout(&self) -> bool {
+    /// Whether or not the callback failed as a result of the 60 second timeout.
+    ///
+    /// Read more [here](Request::reset_timeout).
+    pub fn timeout(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_CANCEL_FLAG_IO_TIMEOUT).0 != 0
     }
 
-    pub fn io_aborted(&self) -> bool {
+    /// The user has cancelled the request manually.
+    ///
+    /// A user could cancel a request through a download toast?
+    pub fn user_cancelled(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_CANCEL_FLAG_IO_ABORTED).0 != 0
     }
 
+    /// The range of data that was supposed to be fetched.
     pub fn file_range(&self) -> Range<u64> {
         let range = unsafe { self.0.Anonymous.FetchData };
         (range.FileOffset as u64)..(range.FileOffset + range.Length) as u64
@@ -80,45 +99,62 @@ struct CancelFetchDataDebug {
     Length: i64,
 }
 
+/// Information for the `SyncFilter::validate_data` callback.
 #[derive(Debug, Clone, Copy)]
 pub struct ValidateData(pub(crate) CF_CALLBACK_PARAMETERS_0_11);
 
 impl ValidateData {
+    /// Whether or not the callback failed as a result of the 60 second timeout.
     pub fn explicit_hydration(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_VALIDATE_DATA_FLAG_EXPLICIT_HYDRATION).0 != 0
     }
 
-    pub fn required_file_range(&self) -> Range<u64> {
+    /// The range of data to validate.
+    pub fn file_range(&self) -> Range<u64> {
         (self.0.RequiredFileOffset as u64)
             ..(self.0.RequiredFileOffset + self.0.RequiredLength) as u64
     }
 }
 
+/// Information for the `SyncFilter::fetch_placeholders` callback.
 #[derive(Debug)]
 pub struct FetchPlaceholders(pub(crate) CF_CALLBACK_PARAMETERS_0_7);
 
 impl FetchPlaceholders {
+    /// A glob pattern specifying the files that should be fetched.
+    ///
+    /// This field is completely optional and does not have to be respected.
     #[cfg(feature = "globs")]
     pub fn pattern(&self) -> Result<globset::Glob, globset::Error> {
         let pattern = unsafe { U16CStr::from_ptr_str(self.0.Pattern.0) }.to_string_lossy();
         globset::Glob::new(&pattern)
     }
 
+    /// A glob pattern specifying the files that should be fetched.
+    ///
+    /// This field is completely optional and does not have to be respected.
     #[cfg(not(feature = "globs"))]
     pub fn pattern(&self) -> &U16CStr {
         unsafe { U16CStr::from_ptr_str(self.0.Pattern.0) }
     }
 }
 
+/// Information for the `SyncFilter::cancel_fetch_placeholders` callback.
 #[derive(Clone, Copy)]
 pub struct CancelFetchPlaceholders(pub(crate) CF_CALLBACK_PARAMETERS_0_0);
 
 impl CancelFetchPlaceholders {
-    pub fn io_timeout(&self) -> bool {
+    /// Whether or not the callback failed as a result of the 60 second timeout.
+    ///
+    /// Read more [here](Request::reset_timeout).
+    pub fn timeout(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_CANCEL_FLAG_IO_TIMEOUT).0 != 0
     }
 
-    pub fn io_aborted(&self) -> bool {
+    /// The user has cancelled the request manually.
+    ///
+    /// A user could cancel a request through a download toast?
+    pub fn user_cancelled(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_CANCEL_FLAG_IO_ABORTED).0 != 0
     }
 }
@@ -139,91 +175,112 @@ struct CancelFetchPlaceholdersDebug {
     Flags: CF_CALLBACK_CANCEL_FLAGS,
 }
 
+/// Information for the `SyncFilter::opened` callback.
 #[derive(Debug, Clone, Copy)]
 pub struct Opened(pub(crate) CF_CALLBACK_PARAMETERS_0_8);
 
 impl Opened {
-    pub fn placeholder_unknown(&self) -> bool {
+    /// The placeholder metadata is corrupt.
+    pub fn metadata_corrupt(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_OPEN_COMPLETION_FLAG_PLACEHOLDER_UNKNOWN).0 != 0
     }
 
-    pub fn placeholder_unsupported(&self) -> bool {
+    /// The placeholder metadata is not supported.
+    pub fn metadata_unsupported(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_OPEN_COMPLETION_FLAG_PLACEHOLDER_UNSUPPORTED).0
             != 0
     }
 }
 
+/// Information for the `SyncFilter::closed` callback.
 #[derive(Debug, Clone, Copy)]
 pub struct Closed(pub(crate) CF_CALLBACK_PARAMETERS_0_1);
 
 impl Closed {
+    /// Whether or not the placeholder was deleted as a result of the close.
     pub fn deleted(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_CLOSE_COMPLETION_FLAG_DELETED).0 != 0
     }
 }
 
+/// Information for the `SyncFilter::dehydrate` callback.
 #[derive(Debug, Clone, Copy)]
 pub struct Dehydrate(pub(crate) CF_CALLBACK_PARAMETERS_0_3);
 
 impl Dehydrate {
+    /// Whether or not the callback was called from a system background service.
     pub fn background(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_DEHYDRATE_FLAG_BACKGROUND).0 != 0
     }
 
-    pub fn reason(&self) -> DehydrationReason {
-        self.0.Reason.into()
+    /// The reason the file is being dehydrated.
+    pub fn reason(&self) -> Option<DehydrationReason> {
+        DehydrationReason::from_win32(self.0.Reason)
     }
 }
 
+/// Information for the `SyncFilter::dehydrated` callback.
 #[derive(Debug, Clone, Copy)]
 pub struct Dehydrated(pub(crate) CF_CALLBACK_PARAMETERS_0_2);
 
 impl Dehydrated {
+    /// Whether or not the callback was called from a system background service.
     pub fn background(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_DEHYDRATE_COMPLETION_FLAG_BACKGROUND).0 != 0
     }
 
-    pub fn dehydrated(&self) -> bool {
+    /// Whether or not the placeholder was already hydrated.
+    pub fn already_hydrated(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_DEHYDRATE_COMPLETION_FLAG_DEHYDRATED).0 != 0
     }
 
-    pub fn reason(&self) -> DehydrationReason {
-        self.0.Reason.into()
+    /// The reason the file is being dehydrated.
+    pub fn reason(&self) -> Option<DehydrationReason> {
+        DehydrationReason::from_win32(self.0.Reason)
     }
 }
 
+/// Information for the `SyncFilter::delete` callback.
 #[derive(Debug, Clone, Copy)]
 pub struct Delete(pub(crate) CF_CALLBACK_PARAMETERS_0_5);
 
 impl Delete {
+    /// Whether or not the placeholder being deleted is a directory.
     pub fn is_directory(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_DELETE_FLAG_IS_DIRECTORY).0 != 0
     }
 
+    // TODO: missing docs
     pub fn is_undelete(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_DELETE_FLAG_IS_UNDELETE).0 != 0
     }
 }
 
+/// Information for the `SyncFilter::deleted` callback.
 #[derive(Debug, Clone, Copy)]
 pub struct Deleted(pub(crate) CF_CALLBACK_PARAMETERS_0_4);
 
+/// Information for the `SyncFilter::rename` callback.
 #[derive(Debug)]
 pub struct Rename(pub(crate) CF_CALLBACK_PARAMETERS_0_10);
 
 impl Rename {
+    /// Whether or not the placeholder being deleted is a directory.
     pub fn is_directory(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_RENAME_FLAG_IS_DIRECTORY).0 != 0
     }
 
+    /// Whether or not the placeholder was originally in the sync root.
     pub fn source_in_scope(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_RENAME_FLAG_SOURCE_IN_SCOPE).0 != 0
     }
 
+    /// Whether or not the placeholder is being moved inside the sync root.
     pub fn target_in_scope(&self) -> bool {
         (self.0.Flags & CloudFilters::CF_CALLBACK_RENAME_FLAG_TARGET_IN_SCOPE).0 != 0
     }
 
+    /// The full path the placeholder is being moved to.
     pub fn target_path(&self) -> PathBuf {
         unsafe {
             U16CStr::from_ptr_str(self.0.TargetPath.0)
@@ -233,10 +290,12 @@ impl Rename {
     }
 }
 
+/// Information for the `SyncFilter::renamed` callback.
 #[derive(Debug)]
 pub struct Renamed(pub(crate) CF_CALLBACK_PARAMETERS_0_9);
 
 impl Renamed {
+    /// The full path the placeholder has been moved from.
     pub fn source_path(&self) -> PathBuf {
         unsafe {
             U16CStr::from_ptr_str(self.0.SourcePath.0)
@@ -246,24 +305,31 @@ impl Renamed {
     }
 }
 
+/// The reason a placeholder has been dehydrated.
 #[derive(Debug, Clone, Copy)]
 pub enum DehydrationReason {
-    UserManual,
-    SystemLowSpace,
-    SystemInactivity,
-    SystemOsUpgrade,
+    /// The user manually dehydrated the placeholder.
+    UserManually,
+    /// The operating system automatically dehydrated the placeholder due to low disk space on the
+    /// volume.
+    LowSpace,
+    /// The operating system automatically dehydrated the placeholder due to low activity.
+    ///
+    /// This is based on the Windows Storage Sense settings.
+    Inactive,
+    /// The operating system automatically dehydrated this file to make room for an operating
+    /// system upgrade.
+    OsUpgrade,
 }
 
-impl From<CF_CALLBACK_DEHYDRATION_REASON> for DehydrationReason {
-    fn from(value: CF_CALLBACK_DEHYDRATION_REASON) -> Self {
-        match value {
-            CloudFilters::CF_CALLBACK_DEHYDRATION_REASON_USER_MANUAL => Self::UserManual,
-            CloudFilters::CF_CALLBACK_DEHYDRATION_REASON_SYSTEM_LOW_SPACE => Self::SystemLowSpace,
-            CloudFilters::CF_CALLBACK_DEHYDRATION_REASON_SYSTEM_INACTIVITY => {
-                Self::SystemInactivity
-            }
-            CloudFilters::CF_CALLBACK_DEHYDRATION_REASON_SYSTEM_OS_UPGRADE => Self::SystemOsUpgrade,
-            _ => unreachable!(),
+impl DehydrationReason {
+    fn from_win32(reason: CF_CALLBACK_DEHYDRATION_REASON) -> Option<DehydrationReason> {
+        match reason {
+            CloudFilters::CF_CALLBACK_DEHYDRATION_REASON_USER_MANUAL => Some(Self::UserManually),
+            CloudFilters::CF_CALLBACK_DEHYDRATION_REASON_SYSTEM_LOW_SPACE => Some(Self::LowSpace),
+            CloudFilters::CF_CALLBACK_DEHYDRATION_REASON_SYSTEM_INACTIVITY => Some(Self::Inactive),
+            CloudFilters::CF_CALLBACK_DEHYDRATION_REASON_SYSTEM_OS_UPGRADE => Some(Self::OsUpgrade),
+            _ => None,
         }
     }
 }
