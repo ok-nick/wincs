410f7da403be93f2541aee843bd13340b85525a2
ok-nick <25470747+ok-nick@users.noreply.github.com>
2022-06-23 19:31:13 -0400

    document modules

--- src/filter/mod.rs
+++ src/filter/mod.rs
@@ -1,6 +1,8 @@
+/// Information for callbacks in the [SyncFilter][crate::SyncFilter] trait.
 pub mod info;
 mod proxy;
 mod sync_filter;
+/// Tickets for callbacks in the [SyncFilter][crate::SyncFilter] trait.
 pub mod ticket;
 
 pub use proxy::{callbacks, Callbacks};
